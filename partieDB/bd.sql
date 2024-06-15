ba8d66d38a4852c7ade7707de48f836bcf8482d4
Thomas Clavé <66850245+thomasclave@users.noreply.github.com>
2024-06-15 23:12:03 +0200

    Ajout des "NOT NULL" + trad en anglais

--- partieDB/bd.sql
+++ partieDB/bd.sql
@@ -1,33 +1,33 @@
--- Création de la base de données
+-- Create database
 CREATE DATABASE IF NOT EXISTS sae24;
 USE sae24;
 
--- Création de la table Salle
+-- Create table Salle
 CREATE TABLE Salle (
-    Nom VARCHAR(30) PRIMARY KEY,
+    NomSalle VARCHAR(30) PRIMARY KEY,
     Bat VARCHAR(5),
-    Longueur INT, 
-    Largeur INT 
+    Longueur INT NOT NULL,
+    Largeur INT NOT NULL
 );
 
--- Création de la table Capteur
+-- Create table Capteur
 CREATE TABLE Capteur (
     IDcapteur INT AUTO_INCREMENT PRIMARY KEY,
-    Type VARCHAR(10),
-    NomSalle VARCHAR(30),
-    Pos_X INT,
-    Pos_Y INT,
-    FOREIGN KEY (NomSalle) REFERENCES Salle(Nom)
+    TypeCapt VARCHAR(10) NOT NULL,
+    NomSalle VARCHAR(30) NOT NULL,
+    Pos_X INT NOT NULL,
+    Pos_Y INT NOT NULL,
+    FOREIGN KEY (NomSalle) REFERENCES Salle(NomSalle)
 );
 
--- Création de la table Data
+-- Create table Data
 CREATE TABLE Data (
     IDdata INT AUTO_INCREMENT PRIMARY KEY,
-    X INT,
-    Y INT,
-    Date DATE,
-    Time TIME,
-    Type VARCHAR(10),
-    NomSalle VARCHAR(30),
-    FOREIGN KEY (NomSalle) REFERENCES Salle(Nom)
+    X INT NOT NULL,
+    Y INT NOT NULL,
+    Date DATE NOT NULL,
+    Time TIME NOT NULL,
+    TypeCapt VARCHAR(10) NOT NULL,
+    NomSalle VARCHAR(30) NOT NULL,
+    FOREIGN KEY (NomSalle) REFERENCES Salle(NomSalle)
 );
