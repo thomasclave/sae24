acf478df1e1e796a27257eb0d2e339fbe3d0001f
romaisimO <simard.romain.iut@gmail.com>
2024-06-17 10:21:58 +0200

    g

--- partieDB/rcpdata.php
+++ partieDB/rcpdata.php
@@ -6,7 +6,7 @@ $idBd = mysqli_connect("localhost", "root", "passroot", "sae23")
     or die("Echec de la connexion à la base de données"); // Connect to the database or display an error message if the connection fails // Connexion à la base de données ou affichage d'un message d'erreur en cas d'échec
 
 while (true) { // Infinite loop to continuously fetch and process data // Boucle infinie pour récupérer et traiter les données en continu
-    $mqtt_broker = "mqtt.iut-blagnac.fr"; // MQTT broker URL // URL du broker MQTT
+    $mqtt_broker = "192.168.104.200"; // MQTT broker URL // URL du broker MQTT
     $mqtt_topic = "AM107/by-room/+/data"; // MQTT topic to subscribe to // Sujet MQTT à s'abonner
 
     // Execute the MQTT subscription command and capture the JSON output // Exécuter la commande d'abonnement MQTT et capturer la sortie JSON
