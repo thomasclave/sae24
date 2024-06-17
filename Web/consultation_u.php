e4e8dcdd13b9ddb12360658f3b5050fe67331d82
Thomas Clavé <thomas.clave14@icloud.com>
2024-06-17 10:30:13 +0200

    Modifications + ajout consultation ultrason

--- Web/consultation_u.php
+++ Web/consultation_u.php
@@ -2,36 +2,29 @@
 include("./script_bdd/mysql.php");
 
 
+
 // Vérification si des données ont été postées depuis le formulaire
 if (isset($_POST["salle"]) && isset($_POST["capteur"])) {
 
     session_start();
 
-    $salle = $_POST["salle"];
-    $capteur = $_POST["capteur"];
+    $salle = $_SESSION["salle"];
+    $capteur = $_SESSION["capteur"];
 
-    $_SESSION['salle'] = $salle;
-    $_SESSION['capteur'] = $capteur;
+    // Requête pour récupérer les dimensions de la salle sélectionnée
+    $requete_dimensions = "SELECT Longueur, Largeur FROM Salle WHERE NomSalle = '$salle'";
+    $resultat_dimensions = mysqli_query($id_bd, $requete_dimensions);
+    $row_dimensions = mysqli_fetch_assoc($resultat_dimensions);
+    $longueur = $row_dimensions['Longueur'];
+    $largeur = $row_dimensions['Largeur'];
 
-    if ($capteur == "Ultrason") {
-        header("Location: consultation_u.php");
-    } else {
-        // Requête pour récupérer les dimensions de la salle sélectionnée
-        $requete_dimensions = "SELECT Longueur, Largeur FROM Salle WHERE NomSalle = '$salle'";
-        $resultat_dimensions = mysqli_query($id_bd, $requete_dimensions);
-        $row_dimensions = mysqli_fetch_assoc($resultat_dimensions);
-        $longueur = $row_dimensions['Longueur'];
-        $largeur = $row_dimensions['Largeur'];
+    // Requête pour récupérer les positions des capteurs
+    $requete_capteurs = "SELECT TypeCapt, Pos_X, Pos_Y FROM Capteur WHERE NomSalle = '$salle' AND TypeCapt = '$capteur'";
+    $resultat_capteurs = mysqli_query($id_bd, $requete_capteurs);
 
-        // Requête pour récupérer les positions des capteurs
-        $requete_capteurs = "SELECT TypeCapt, Pos_X, Pos_Y FROM Capteur WHERE NomSalle = '$salle' AND TypeCapt = '$capteur'";
-        $resultat_capteurs = mysqli_query($id_bd, $requete_capteurs);
-
-        // Requête pour récupérer la derniere position de la personne
-        $requete_personne = "SELECT X, Y FROM Data WHERE NomSalle = '$salle' AND TypeCapt = '$capteur' ORDER BY Date DESC, Time DESC LIMIT 1";
-        $resultat_personne = mysqli_query($id_bd, $requete_personne);
-    }
-
+    // Requête pour récupérer la derniere position de la personne
+    $requete_personne = "SELECT X, Y FROM Data WHERE NomSalle = '$salle' ORDER BY Date DESC, Time DESC LIMIT 1";
+    $resultat_personne = mysqli_query($id_bd, $requete_personne);
     
 }
 ?>
@@ -69,41 +62,15 @@ if (isset($_POST["salle"]) && isset($_POST["capteur"])) {
     </header>
 
     <main>
-        <form action="consultation.php" method="POST">
-            <label for="salle">Sélectionnez une salle :</label>
-            <select name="salle" id="salle" required>
-                <option value="">Choisissez une salle</option>
-                <?php
-                // Affichage des options de sélection des salles
-                $requete_salles = "SELECT NomSalle FROM Salle";
-                $resultat_salles = mysqli_query($id_bd, $requete_salles);
-                while ($row = mysqli_fetch_assoc($resultat_salles)) {
-                    $nomSalle = $row['NomSalle'];
-                    $selected = ($nomSalle == $salle) ? "selected" : "";
-                    echo "<option value='$nomSalle' $selected>$nomSalle</option>";
-                }
-                ?>
-            </select>
-            <br><br>
 
-            <label for="capteur">Sélectionnez un type de capteur :</label>
-            <select name="capteur" id="capteur" required>
-                <option value="Son">Son</option>
-                <option value="UltraSon">Ultrason</option>
-            </select>
-            <br><br>
-
-            <input type="submit" value="Consulter">
-        </form>
-
-        <br><br>
+     <br><br>
 
         <?php
         if (isset($_POST["salle"]) && isset($_POST["capteur"])) {
             if ($resultat_dimensions && mysqli_num_rows($resultat_dimensions) > 0) {
 
             // Tableau pour afficher les positions des capteurs et de la personne
-            echo "<h2>Positions ".$capteur." dans la salle ".$salle." :</h2>";
+            echo "<h2>Positions dans la salle</h2>";
             echo "<table class='salle-table'>";
             // Affichage des cases vides selon les dimensions de la salle
             for ($y = 1; $y <= $largeur; $y++) {
