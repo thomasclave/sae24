87815f3f44f11e2091a0d1a5e98dee24221ba889
Thomas Clavé <thomas.clave14@icloud.com>
2024-06-18 11:51:32 +0200

    MAJ @IP VM

--- Web/script_bdd/mysql.php
+++ Web/script_bdd/mysql.php
@@ -1,7 +1,7 @@
 <?php
 /* Connection Script to database SQL */
 
-  $id_bd = mysqli_connect("192.168.102.200","g31","passg31","sae24")
+  $id_bd = mysqli_connect("192.168.102.250","g31","passg31","sae24")
     or die("Connexion au serveur et/ou à la base de données impossible");
 
   /* Character encoding management */
