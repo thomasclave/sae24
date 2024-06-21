36cf5bdd721ce5af0c195868aff81040bfb386c2
Thomas Clavé <thomas.clave14@icloud.com>
2024-06-21 08:42:19 +0200

    Ajout de la considération de case bleue

--- Web/scripts/bouton_reset.js
+++ Web/scripts/bouton_reset.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     // Adding an event handler for the reset button
     document.getElementById("reset-button").addEventListener("click", function() {
         // Get all the <td> elements of the table
-        var cases = document.querySelectorAll(".salle-table td.case_grise, .salle-table-ultra td.case_grise_ultra");
+        var cases = document.querySelectorAll(".salle-table td.case_grise , .salle-table-ultra td.case_grise_ultra, .salle-table td.case_bleue, .salle-table-ultra td.case_bleue_ultra");
 
         // Iterate through each gray case and transform it into an empty case
         cases.forEach(function(caseElement) {
